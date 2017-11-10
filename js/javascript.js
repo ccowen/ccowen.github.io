09c2d728d5ac421a6fa556139a31e5aded982ab5
ccowen <caitlincowen2016@u.northwestern.edu>
2017-11-10 14:33:15 -0600

    second modal, css

--- js/javascript.js
+++ js/javascript.js
@@ -9,9 +9,10 @@ function newDraggable() {
         containment: "parent"
     }).on('dragstart', function (e, ui) {
         $(ui.helper).css('z-index','auto');
-        $(ui.helper).attr('class', 'expressions expressions' + i);
+        $(ui.helper).attr('class', 'expressions expressions' + i);        
         console.log(i);        
     }).on('dragstop', function (e, ui) {
+
         var expressions = $(this).after($(ui.helper).clone());
         i++;
         if (i == 6){
@@ -34,7 +35,7 @@ $(document).ready( function () {
 
 window.onload = function(){ 
     // Get the modal
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('instructionsModal');
 
     // Get the button that opens the modal
     var btn = document.getElementById("instructionsBtn");
@@ -56,6 +57,33 @@ window.onload = function(){
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+        }
+    }
+
+    // second modal ------------------------
+    // Get the modal
+    var modal2 = document.getElementById('infoModal');
+
+    // Get the button that opens the modal
+    var btn2 = document.getElementById("infoBtn");
+
+    // Get the <span> element that closes the modal
+    var span2 = document.getElementsByClassName("close2-mycss")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn2.onclick = function() {
+        modal2.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span2.onclick = function() {
+        modal2.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
         }
     }
 };
