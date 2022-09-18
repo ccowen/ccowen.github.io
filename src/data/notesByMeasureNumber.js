48eae41a242a0e472d603aaa5b21158d854d6f17
ccowen <caitlincowen2016@u.northwestern.edu>
2022-09-18 15:51:31 -0500

    add text notes by measure number data

---
+++ src/data/notesByMeasureNumber.js
@@ -0,0 +1,17 @@
+const notesByMeasureNumber = {
+    'active_measures': [32, 33, 34, 35, 36, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67],
+    'records': [
+        {
+            "note": 'The vocal sun represnting the sample from "With You" and instrument sun representing "Viva la Vida" are on the same spot',
+            "measures": [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67]
+        },
+        {
+            "note": "details on the suns not disappearing completely are in the section 'Extending samples that aren't heard, m32-36'",
+            "measures": [32, 33, 34, 35, 36]
+        }
+    ]
+}
+
+
+
+export default notesByMeasureNumber
