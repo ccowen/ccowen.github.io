fb7667630c3ca68d6c54e4fc07809eb589823711
ccowen <caitlincowen2016@u.northwestern.edu>
2022-09-18 15:52:30 -0500

    add data for extended lyrics m32-36

---
+++ src/data/lyricsExtendedm31to36.js
@@ -0,0 +1,46 @@
+const lyricsExtendedm31to36 = { 
+    "No Air": {
+        "32": "do you expect", 
+        "33": "me to live alone with just",
+        "34": "me",
+        "35": "hard for me to breathe",
+        "36": "",
+    },
+    "No Air Extended": {
+        "32": "", 
+        "33": "",
+        "34": "cause my world revolves a-",
+        "35": "-round you it's so",
+        "36": "",
+    },
+    "Love Song": {
+        "32": "", 
+        "33": "",
+        "34": "breathing gets",
+        "35": "harder .. even",
+        "36": "I know that",
+    },
+    "Love Song Extended": {
+        "32": "", 
+        "33": "",
+        "34": "",
+        "35": "",
+        "36": "",
+    },
+    "Bubbly": {
+        "32": "", 
+        "33": "",
+        "34": "got me feeling like a child now",
+        "35": "",
+        "36": "",
+    },
+    "Bubbly Extended": {
+        "32": "", 
+        "33": "",
+        "34": "",
+        "35": "every time I see your bubbly face",
+        "36": "get the tingles in a silly place",
+    }
+}
+
+export default lyricsExtendedm31to36
