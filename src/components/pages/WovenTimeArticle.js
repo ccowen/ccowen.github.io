4e95771049de6232d4794ad16b172e190413b226
ccowen <caitlincowen2016@u.northwestern.edu>
2024-12-27 14:26:28 -0600

    formatting

--- src/components/pages/WovenTimeArticle.js
+++ src/components/pages/WovenTimeArticle.js
@@ -3,12 +3,12 @@ import ArticleWrapper from "../ArticleWrapper";
 import contents from "../../directory/woven-time/content.json"
 import directory from "../../directory/contentDirectory.json"
 
-function WovenTimeArticleNew() {
+function WovenTimeArticle() {
     const headers = directory["content-directory"]["woven-time"]
 
     return (
         <ArticleWrapper headers={headers} contents={contents.content} />
     );
-  }
+}
   
-  export default WovenTimeArticleNew;
+export default WovenTimeArticle
