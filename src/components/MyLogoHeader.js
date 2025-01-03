a88842971435d7b5b8e9597e37f19f740790d9b2
ccowen <caitlincowen2016@u.northwestern.edu>
2025-01-02 18:02:34 -0600

    wrap logo header with appbar

--- src/components/MyLogoHeader.js
+++ src/components/MyLogoHeader.js
@@ -1,25 +1,29 @@
+import { AppBar } from "@mui/material";
+
 function MyLogoHeader() {
     return(
-      <a 
-         href="/"
-         style={{
-            display:'block',
-            height: '75px',
-            width: '291px',
-         }}
-      >
-         <div
-            title="Caitlin Cowen portfolio header"
+      <AppBar position="sticky" color="transparent">
+         <a 
+            href="/"
             style={{
+               display:'block',
                height: '75px',
                width: '291px',
-               backgroundImage:  "url('/portfolio-header.png')",
-               backgroundSize: "100%",
-               backgroundRepeat: "no-repeat",
-               backgroundPosition: "center",
             }}
-         />
-      </a>
+         >
+            <div
+               title="Caitlin Cowen portfolio header"
+               style={{
+                  height: '75px',
+                  width: '291px',
+                  backgroundImage:  "url('/portfolio-header.png')",
+                  backgroundSize: "100%",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+               }}
+            />
+         </a>
+      </AppBar>
 
      );
 }
