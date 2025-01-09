e3953bf7ddf7dcd07d286477a439e4a71599eab4
ccowen <caitlincowen2016@u.northwestern.edu>
2025-01-09 09:04:36 -0600

    mobile responsive home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,16 +2,16 @@ import { Stack } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import MyLogoHeader from "../MyLogoHeader";
 import HomeLeftPanel from "./home/HomeLeftPanel";
-import HomeRightPanel from "./home/HomeRightPanel";
-
+import MyContentWidget from "./../MyContentWidget"
 import myColors from "../../myColors";
+
 function HomePage() {
 
   return (
     <>
         <Grid container spacing={0}>
             <Grid 
-                size={7}
+                size={{lg:7, md:5, sm:12 }}
                 sx={{ backgroundColor: myColors.primary.light, minHeight: '100vh'}}
             >
                 <Stack
@@ -29,7 +29,7 @@ function HomePage() {
                 </Stack>
 
             </Grid>
-            <Grid size={5} sx={{ backgroundColor: myColors.whiteGray }}>
+            <Grid size={{lg:5, md:7, sm:12 }} sx={{ backgroundColor: myColors.whiteGray }}>
                 <Stack
                     direction={'column'}
                     sx={{
@@ -38,7 +38,9 @@ function HomePage() {
                         height: '100%'
                     }}
                 >
-                    <HomeRightPanel/>
+                    <Grid sx={{margin: '64px'}}>
+                        <MyContentWidget/>
+                    </Grid>
                 </Stack>
             </Grid>
         </Grid>
